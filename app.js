b23e0948dfc3ce9d3c582061b1b8e095435f12d7
andrewbalberchak <abalberchak@gmail.com>
2018-02-13 22:17:32 -0800

    2nd commit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var express 		= require('express');
-var bodyParser 	= require('body-parser');
-var path 				= require('path');
+var express 				 = require('express');
+var bodyParser 			 = require('body-parser');
+var path 						 = require('path');
+var expressValidator = require('express-validator');
+var mongojs 				 = require('mongojs');
+
+//needed to talk to DB
+var db = mongojs('customerapp', ['users']);
 
 var app = express();
 var port = 3000;
@@ -23,6 +28,32 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 /////////////// Set Static Path ///////////////
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+/////////////// Global Vars ///////////////
+app.use(function(req, res, next) {
+	res.locals.errors = null;
+	next();
+});
+
+
+/////////////// Express Validator Middleware ///////////////
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+		while (namespace.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+}));
+
 
 
 /////////////// Dummy Data ///////////////
@@ -51,10 +82,48 @@ var users = [
 app.get('/', function(req, res){
 	// res.send('Hello Grunt!');
 	// res.json(people);
-	res.render('index', {
-		title: 'Customers',
-		users: users
+
+	// Needed to talk to DB.
+	db.users.find(function (err, docs) {
+		console.log(docs);
+		res.render('index', {
+			title: 'Customers',
+			users: docs
+			//users points to docs to retrieve from DB.
+		});
 	});
+});
+
+app.post('/users/add', function(req, res) {
+
+	req.checkBody('first_name', 'First Name is Required').notEmpty();
+	req.checkBody('last_name', 'Last Name is Required').notEmpty();
+	req.checkBody('email', 'Email is Required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if (errors){
+		res.render('index', {
+			title: 'Customers',
+			users: users,
+			errors: errors
+		});
+		console.log(errors);
+	} else {
+		var newUser = {
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+			email: req.body.email
+		};
+		db.users.insert(newUser, function(err, result) {
+			if (err) {
+				console.log(err);
+			} else {
+				res.redirect('/');
+			}
+		});
+	}
+
 });
 
 
